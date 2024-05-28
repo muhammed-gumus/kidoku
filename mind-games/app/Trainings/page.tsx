6a9877fd507139b7d9b7bb32f4c111d370b2a5b9
muhammed-gumus <mr.silver.mg@gmail.com>
2024-05-28 17:28:28 +0300

    fix(app): fixed trainings page responsive problem

--- mind-games/app/Trainings/page.tsx
+++ mind-games/app/Trainings/page.tsx
@@ -2,14 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center mt-24">
+    <div className="flex min-h-screen flex-col items-center justify-center px-4">
       <Image
         src="/images/logo-notbg.png"
         width={200}
         height={200}
         alt="Picture of the author"
+        className="w-48 h-48 md:w-52 md:h-52 lg:w-64 lg:h-64"
       />
-      <h1 className="font-extrabold text-8xl mt-8">YAKINDA HİZMETİNİZDE...</h1>
+      <h1 className="font-extrabold text-4xl md:text-6xl lg:text-8xl text-center">
+        YAKINDA HİZMETİNİZDE...
+      </h1>
     </div>
   );
 }
